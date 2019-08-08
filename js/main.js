e5552ead02e707fe1370fbdf56f9e07262b2d8ba
siladigy <sgeorge7731@gmail.com>
2019-08-08 18:02:47 +0300

    constructor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // popup-call
 
+(() => {
 const popupCall = document.querySelector('.popup-call'),
       callBtn = document.querySelectorAll('.call-btn');
 
@@ -15,9 +16,10 @@ popupCall.addEventListener('click', (event) => {
         popupCall.style.display = "none";
     }
 });
+})();
 
 //popup-check
-
+(() => {
 const popupCheck = document.querySelector('.popup-check'),
       checkBtn = document.querySelector('.check-btn');
 
@@ -31,9 +33,10 @@ popupCheck.addEventListener('click', (event) => {
         popupCheck.style.display = "none";
     }
 });
+})();
 
 //sentence
-
+(() => {
 const  addSentence = document.querySelector('.add-sentence-btn'),
        sentenceBlock = document.querySelectorAll('.sentence-block');
 
@@ -46,9 +49,11 @@ addSentence.addEventListener('click', () => {
         
     });
 });
+})();
 
 //popup-discount
 
+(() => {
 const popupDiscount = document.querySelector('.popup-discount'),
       discountBtn = document.querySelectorAll('.discount-btn');
 
@@ -65,19 +70,61 @@ popupDiscount.addEventListener('click', (event) => {
         popupDiscount.style.display = "none";
     }
 });
+})();
+
 
 //questions
 
-const panelHeading = document.querySelectorAll('.panel-heading'),
-      panelCollapse = document.querySelectorAll('.panel-collapse');
+(() => {
+const question = document.querySelector('.questions'),
+      panelHeading = question.querySelectorAll('.panel-heading'),
+      panelCollapse = question.querySelectorAll('.panel-collapse'),
+      links = question.querySelectorAll('a');
+
+      links.forEach((elem) => {
+        elem.href = "#!";
+      });
 
 for (let i = 0; i<panelHeading.length; i++){
-    panelHeading[i].addEventListener('click', (event) => {
+    panelHeading[i].addEventListener('click', () => {
         panelCollapse.forEach((elem) => {
             elem.classList.remove('in');
         });
         panelCollapse[i].classList.add('in');
     });
 }
-    
+})();
+
+
+//constructor
+
+(() => {
+const constructor = document.querySelector('.constructor'),
+      panelHeading = constructor.querySelectorAll('.panel-heading'),
+      panelCollapse = constructor.querySelectorAll('.panel-collapse'),
+      constructBtn = constructor.querySelectorAll('.construct-btn'),
+      links = constructor.querySelectorAll('a');
+
+      links.forEach((elem) => {
+        elem.href = "#!";
+      });
+
+      for (let i = 0; i<panelHeading.length; i++){
+        panelHeading[i].addEventListener('click', () => {
+            panelCollapse.forEach((elem) => {
+                elem.classList.remove('in');
+            });
+            panelCollapse[i].classList.add('in');
+        });
+        constructBtn[i].addEventListener('click', () => {
+            panelCollapse.forEach((elem) => {
+                elem.classList.remove('in');
+            });
+            if(i !== 3){
+                panelCollapse[i+1].classList.add('in');
+            }
+        });
+    }
+
+})();
 
