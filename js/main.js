b65d25c038e2f716eb7a3763e7ff9b2bf9168ab4
siladigy <sgeorge7731@gmail.com>
2019-08-08 11:07:05 +0300

    popup check

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 // popup-call
 
 const popupCall = document.querySelector('.popup-call'),
-      callBtn = document.querySelectorAll('.call-btn'),
-      closeBtn = popupCall.querySelector('.popup-close');
+      callBtn = document.querySelectorAll('.call-btn');
 
 callBtn.forEach((elem) => {
     elem.addEventListener('click', () => {
@@ -14,5 +13,21 @@ popupCall.addEventListener('click', (event) => {
     let target = event.target;
     if(target.classList.contains('popup-close') || target.classList.contains('popup')){
         popupCall.style.display = "none";
+    }
+});
+
+//popup-check
+
+const popupCheck = document.querySelector('.popup-check'),
+      checkBtn = document.querySelector('.check-btn');
+
+checkBtn.addEventListener('click', () => {
+    popupCheck.style.display = "block";
+});
+
+popupCheck.addEventListener('click', (event) => {
+    let target = event.target;
+    if(target.classList.contains('popup-close') || target.classList.contains('popup')){
+        popupCheck.style.display = "none";
     }
 });
